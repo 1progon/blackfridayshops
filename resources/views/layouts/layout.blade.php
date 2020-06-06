23824cebe589c5555754457dfde7d8a8280e0a0c
Alex <gageviolo@gmail.com>
2020-06-06 19:15:36 +0500

    Добавлена стрелка категорий для мобильной версии в зависимости от состояния

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html lang="ru">
 <head>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -9,7 +11,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="{{ asset('css/main.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
+
 
     @yield('head')
 
@@ -25,8 +29,13 @@
 
 <div id="wrapper" class="d-flex flex-wrap">
 
-    <button v-on:click="showCats = !showCats" id="show-sidebar-btn" type="button" class="btn btn-primary m-2">Показать
-        категории
+    <button v-on:click="showCats = !showCats"
+            id="show-sidebar-btn"
+            type="button"
+            class="btn btn-primary m-2 btn-block">
+        Категории
+        <img v-if="showCats" src="{{ asset('imgs/down-chevron.svg') }}" alt="" width="16" height="16">
+        <img v-else src="{{ asset('imgs/up-chevron.svg') }}" alt="" width="16" height="16">
     </button>
 
     <aside v-bind:style="showCats ? 'display: unset' : ''" id="sidebar" class="col-12 col-sm-2 p-0">
@@ -45,9 +54,6 @@
 
 
 </div>
-
-
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 
 
 <script src="{{ asset('js/vue-ctrl.js') }}"></script>
