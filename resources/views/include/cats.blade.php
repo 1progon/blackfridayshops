992aa993405365b240ab13b530fc1e82d465bc34
Alex <gageviolo@gmail.com>
2020-06-07 18:39:58 +0500

    JS: Добавлена прокрутка до активного элемента в сайдбаре категорий, когда страница полностью загружена

--- resources/views/include/cats.blade.php
+++ resources/views/include/cats.blade.php
@@ -4,18 +4,16 @@
     $cats = (new CategoriesController())->index(true)->sortBy('position');
 @endphp
 
+
 <div id="categories" class="d-flex list-group rounded-0">
     @forelse( $cats as $cat)
-        <a class="list-group-item list-group-item-dark list-group-item-action font-weight-bold {{ url()
-            ->current()
-             == route
-            ('category', $cat->slug)
-            ?'active': ''}}"
+        <a class="list-group-item list-group-item-dark list-group-item-action font-weight-bold
+        {{ url()->current()== route('category', $cat->slug)?'active': ''}}"
            href="{{ route('category', [$cat->slug])}}">{{ $cat->name}}</a>
-        <div class="d-flex flex-wrap rounded-0 ">
+        <div class="d-flex flex-wrap rounded-0">
             @forelse( $cat->subCategories as $subCat)
                 <a class="list-group-item list-group-item-secondary list-group-item-action py-0
-{{ url()->current() == route('category',[$cat->slug, $subCat->slug]) ?'active': ''}}"
+                {{ url()->current() == route('category',[$cat->slug, $subCat->slug]) ?'active': ''}}"
                    href="{{ route('category',[$cat->slug, $subCat->slug])}}">
                     {{ $subCat->name}}
                 </a>
@@ -28,3 +26,17 @@
     @endforelse
 
 </div>
+
+
+<script>
+    document.addEventListener('DOMContentLoaded', scrollToActiveCategory);
+
+    function scrollToActiveCategory() {
+        let activeElement = document.querySelector('#categories .active');
+        console.log(activeElement);
+
+        if (activeElement) {
+            activeElement.scrollIntoView({behavior: "smooth"});
+        }
+    }
+</script>
