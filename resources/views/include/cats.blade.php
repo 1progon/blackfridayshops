cd6683dfe1aa61385a5285b4394c7ec85697e1db
Alex <gageviolo@gmail.com>
2020-06-07 17:44:58 +0500

    Добавлен значок прокрутки для категорий

--- resources/views/include/cats.blade.php
+++ resources/views/include/cats.blade.php
@@ -4,20 +4,20 @@
     $cats = (new CategoriesController())->index(true)->sortBy('position');
 @endphp
 
-<div class="d-flex list-group rounded-0">
-    @forelse($cats as $cat)
-        <a class="list-group-item list-group-item-dark list-group-item-action font-weight-bold {{url()
+<div id="categories" class="d-flex list-group rounded-0">
+    @forelse( $cats as $cat)
+        <a class="list-group-item list-group-item-dark list-group-item-action font-weight-bold {{ url()
             ->current()
              == route
             ('category', $cat->slug)
             ?'active': ''}}"
-           href="{{route('category', [$cat->slug])}}">{{$cat->name}}</a>
+           href="{{ route('category', [$cat->slug])}}">{{ $cat->name}}</a>
         <div class="d-flex flex-wrap rounded-0 ">
-            @forelse($cat->subCategories as $subCat)
+            @forelse( $cat->subCategories as $subCat)
                 <a class="list-group-item list-group-item-secondary list-group-item-action py-0
-{{url()->current() == route('category',[$cat->slug, $subCat->slug]) ?'active': ''}}"
-                   href="{{route('category',[$cat->slug, $subCat->slug])}}">
-                    {{$subCat->name}}
+{{ url()->current() == route('category',[$cat->slug, $subCat->slug]) ?'active': ''}}"
+                   href="{{ route('category',[$cat->slug, $subCat->slug])}}">
+                    {{ $subCat->name}}
                 </a>
             @empty
             @endforelse
