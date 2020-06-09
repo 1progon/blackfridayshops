e7f62d8d30a1dce02b2c9c3527662ac2918b6f23
Alex <gageviolo@gmail.com>
2020-06-09 12:37:18 +0500

    Изменения в дизайне значка (открыть в новом окне) на кнопке перейти в магазин

--- resources/views/shop/card.blade.php
+++ resources/views/shop/card.blade.php
@@ -1,8 +1,8 @@
-<a href="{{route('shop', [$shop->slug])}}"
-   class="col-6 col-md-2 my-card m-0 m-sm-1 p-1"
+<a href="{{ route('shop', [$shop->slug])}}"
+   class="col-6 col-sm-6 col-md-3 col-lg-2 my-card m-0 p-0 m-lg-1 p-lg-1"
    style="flex-grow: 1">
     <div>
-        <img src="{{$shop->adm_image}}" class="" alt="{{$shop->name}}" width="143" height="59">
+        <img style="max-width: 100%" src="{{$shop->adm_image}}" class="" alt="{{$shop->name}}" width="143" height="59">
         <div class="py-2">
             <h5 style="word-break: break-all" class="card-title">{{$shop->name}}</h5>
             <p class="card-text">{{$shop->description}}</p>
