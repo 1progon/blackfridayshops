08b98511cafcc8591ead65061df3a5d1c463de7c
Alex <gageviolo@gmail.com>
2020-10-26 18:43:17 +0500

    Commented header add shop menu

--- resources/views/include/add-site.blade.php
+++ resources/views/include/add-site.blade.php
@@ -1,20 +1,20 @@
-<form id="add-site" @submit.prevent="submitFormAddSite">
-    <div class="input-group">
-        <label for="site-name">Имя сайта</label>
-        <input type="text" name="site_name" id="site-name">
-    </div>
+{{--<form id="add-site" @submit.prevent="submitFormAddSite">--}}
+{{--    <div class="input-group">--}}
+{{--        <label for="site-name">Имя сайта</label>--}}
+{{--        <input type="text" name="site_name" id="site-name">--}}
+{{--    </div>--}}
 
-    <div class="input-group">
-        <label for="site-author">Создатель автор</label>
-        <input type="text" name="site_author" id="site-author">
-    </div>
+{{--    <div class="input-group">--}}
+{{--        <label for="site-author">Создатель автор</label>--}}
+{{--        <input type="text" name="site_author" id="site-author">--}}
+{{--    </div>--}}
 
-    <div class="input-group">
-        <label for="site-link">Ссылка на сайт</label>
-        <input type="text" name="site_link" id="site-link">
-    </div>
+{{--    <div class="input-group">--}}
+{{--        <label for="site-link">Ссылка на сайт</label>--}}
+{{--        <input type="text" name="site_link" id="site-link">--}}
+{{--    </div>--}}
 
-    <div class="input-group">
-        <input type="submit" name="" id="" value="Добавить">
-    </div>
-</form>
+{{--    <div class="input-group">--}}
+{{--        <input type="submit" name="" id="" value="Добавить">--}}
+{{--    </div>--}}
+{{--</form>--}}
--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -35,25 +35,6 @@
             </li>
         @endauth
 
-
-
-        {{--        <li class="nav-item">--}}
-        {{--            <a @click.prevent="addShop" href="#" class="nav-link add-shop-button">Добавить свой</a>--}}
-        {{--        </li>--}}
     </ul>
 
-
-    <div>
-        {{--        @guest--}}
-        {{--            <a class="btn btn-light" href="/login">Логин</a>--}}
-        {{--            <a class="btn btn-light" href="/register">Регистрация</a>--}}
-        {{--        @else--}}
-        {{--            <a class="btn btn-light" href="/home">Аккаунт</a>--}}
-        {{--            <input class="btn btn-light" type="submit" name="" id="" value="Выйти" form="logout">--}}
-
-        {{--            <form id="logout" action="/logout" method="post">--}}
-        {{--                @csrf--}}
-        {{--            </form>--}}
-        {{--        @endguest--}}
-    </div>
 </nav>
